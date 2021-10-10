846d2ba60eb2e2bce4991802180e6d4cea709a52
Sarowar Hosen <sarowarhosenakib2@gmail.com>
2021-10-10 12:16:41 +0600

    Fixed SpaceXList component defaul image

--- src/components/SpaceXList.js
+++ src/components/SpaceXList.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import defaultImage from "../assets/images/dafult_image.png"
+
 const SpaceXLists = ({ spaceX }) => {
 
     return (
         <div className="col-md-4">
             <div className="card border mb-5">
-                <img src={spaceX.links.mission_patch_small} alt="Mission Logo" className="card-img-top p-4" width="250" height="280"/>
+                <img
+                    src={spaceX.links.mission_patch_small ? spaceX.links.mission_patch_small: defaultImage}
+                    className="card-img-top p-4"
+                    alt="Mission Logo"
+                    width="250"
+                    height="280"
+                />
                 <div className="card-body">
                     <Link to={`/mission/${spaceX.mission_name}`} className="card-title h6">{spaceX.mission_name}</Link>
                     <p className="my-1"><b>Mission id: </b> {spaceX.mission_id.length ? spaceX.mission_id : "No ID"}</p>
